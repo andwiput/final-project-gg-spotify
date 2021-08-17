75ae7d7452aabf07073674872458df9c776d135c
andwiput <andyginting94@gmail.com>
2021-08-18 03:41:34 +0700

    make a landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Box, Button, Center, Flex, Grid, Heading, Text, Image} from "@chakra-ui/react";
 
 
 function App() {
   return (
-    <div>
-      
-    </div> 
+    <Box maxW="100vw" p="2rem">
+      <Flex justify={'space-between'} align={'center'}>
+        <Text fontSize={'2xl'} fontWeight={900}>GenG Music</Text>
+        <Button>Login with Spotify</Button>
+      </Flex>
+      <Grid templateColumns="1fr 1fr" gap="5vw" mt="3rem">
+        <Flex direction={'column'} justify={'center'}>
+          <Heading as={'h1'} fontSize={'6xl'}> Welcome to GenG Music!</Heading>
+          <Text my={4} color={'smoke.400'}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius unde vel cumque natus voluptates deleniti totam tempore, doloribus laudantium mollitia. Soluta iure, excepturi repellendus fugiat at provident reprehenderit nesciunt?</Text>
+          <Box>
+            <Button size={'md'}>Login with Spotify</Button>
+          </Box>
+        </Flex>
+        <Center>
+          <Image src={'https://picsum.photos/400'}/>
+        </Center>
+      </Grid>
+    </Box>
   );
 }
 
