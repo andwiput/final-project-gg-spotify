166f5deb13f04d65c96374b2be469ceaa6f86fdb
andwiput <andyginting94@gmail.com>
2021-08-18 21:31:15 +0700

    add chakra icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import { Box, Button, Center, Flex, Grid, Heading, Text, Image} from "@chakra-ui
 
 function App() {
   return (
-    <Box maxW="100vw" p="2rem">
+    <Box maxW="100vw">
       <Flex justify={'space-between'} align={'center'}>
         <Text fontSize={'2xl'} fontWeight={900}>GenG Music</Text>
-        <Button>Login with Spotify</Button>
       </Flex>
-      <Grid templateColumns="1fr 1fr" gap="5vw" mt="3rem">
-        <Flex direction={'column'} justify={'center'}>
+      <Grid templateColumns="1fr 1fr" gap="5vw">
+        <Flex direction={'column'} justify={'center'} p="2rem">
           <Heading as={'h1'} fontSize={'6xl'}> Welcome to GenG Music!</Heading>
           <Text my={4} color={'smoke.400'}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit eius unde vel cumque natus voluptates deleniti totam tempore, doloribus laudantium mollitia. Soluta iure, excepturi repellendus fugiat at provident reprehenderit nesciunt?</Text>
           <Box>
