8c0fbc6c229739fdb8d34fd1c1d91511388a386d
andwiput <andyginting94@gmail.com>
2021-08-19 22:45:16 +0700

    fixing refactor and implement `react-router`

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { useAppSelector } from './store';
 import LandingPage from './pages/landingPage'
+import Header from './components/header';
+import CreatePlaylist from './pages/createPlaylist';
 
 function App() {
+  const { isAuth } = useAppSelector(state => state.auth)
+
+
   return (
-    <LandingPage/>
+    <BrowserRouter>
+      <Header />
+      <main>
+        {/* <Sidebar /> */}
+        <Switch>
+          <Route exact path="/create-playlist">
+            {isAuth ? <CreatePlaylist /> : <Redirect to="/" /> }
+          </Route>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
