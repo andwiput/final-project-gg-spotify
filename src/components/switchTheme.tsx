d6e2f866ea9eee32c118595ddab06b9d9a8a718f
andwiput <andyginting94@gmail.com>
2021-08-19 22:39:37 +0700

    made switch theme button

--- src/components/switchTheme.tsx
+++ src/components/switchTheme.tsx
@@ -1,9 +1,15 @@
-import { Switch, useColorMode } from "@chakra-ui/react"
+import { MoonIcon, SunIcon } from "@chakra-ui/icons"
+import { Flex, Icon, Switch, useColorMode } from "@chakra-ui/react"
 
-const SwitchTheme = () =>{
+const SwitchTheme = ({withIcon = false}: {withIcon?:boolean}) =>{
     const { colorMode, toggleColorMode } = useColorMode()
 
-    return <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
+    return(
+        <Flex>
+            <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode}/>
+            {withIcon && <Icon ml={4} as={colorMode === 'dark' ? MoonIcon : SunIcon}></Icon>}
+        </Flex>
+    ) 
 }
 
 export default SwitchTheme
