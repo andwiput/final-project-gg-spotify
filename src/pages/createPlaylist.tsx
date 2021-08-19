a859bc13c2ba1b94b11c947db9406cc174503f0d
andwiput <andyginting94@gmail.com>
2021-08-19 22:42:49 +0700

    add create playlist page

---
+++ src/pages/createPlaylist.tsx
@@ -0,0 +1,47 @@
+import { Box, Flex, Center, Heading, Button, useDisclosure } from '@chakra-ui/react'
+import { AddIcon, DeleteIcon } from '@chakra-ui/icons'
+import { useAppDispatch, useAppSelector } from '../store'
+import { clearSelectedTracks } from '../store/playlist'
+import TrackList from '../components/trackList'
+import ModalCreatePlaylist from '../components/modalCreatePlaylist'
+
+const CreatePlaylist = () => {
+  const dispatch = useAppDispatch()
+  const { selectedTracks } = useAppSelector(state => state.playlist)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  return (
+    <Box w={'100%'} p={'2rem'}>
+      <Flex align="center" justify="space-between">
+        <Heading as="h2" size="lg">
+          Create Playlist
+        </Heading>
+        <Center>
+          <Button
+            leftIcon={<DeleteIcon />}
+            onClick={() => dispatch(clearSelectedTracks())}
+            type="button"
+            variant="ghost"
+            disabled={selectedTracks.length === 0}
+            size={'sm'}
+          >
+            Clear Selection
+          </Button>
+          <Button
+            leftIcon={<AddIcon />}
+            onClick={onOpen}
+            disabled={selectedTracks.length === 0}
+            ml={2}
+            size={'sm'}
+          >
+            Create Playlist
+          </Button>
+        </Center>
+      </Flex>
+      <TrackList />
+      <ModalCreatePlaylist isOpen={isOpen} onClose={onClose} />
+    </Box>
+  )
+}
+
+export default CreatePlaylist
