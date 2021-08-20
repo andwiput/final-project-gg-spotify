eb3d78e2b5d5e1d5417127fb6b5f02fd5d0cfc39
andwiput <andyginting94@gmail.com>
2021-08-20 09:39:39 +0700

    fix route home and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { useAppSelector } from './store';
-import LandingPage from './pages/landingPage'
 import Header from './components/header';
+import Sidebar from './components/sideBar';
+import LandingPage from './pages/landingPage'
+import Home from './pages/home';
 import CreatePlaylist from './pages/createPlaylist';
+import { Grid } from '@chakra-ui/layout';
 
 function App() {
   const { isAuth } = useAppSelector(state => state.auth)
@@ -11,17 +14,22 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <main>
-        {/* <Sidebar /> */}
+      <Grid 
+        as={'main'} 
+        templateColumns= {isAuth ? '1fr 4fr' : '1fr'}>
+        {isAuth && <Sidebar />}
         <Switch>
+          <Route exact path="/home">
+            {isAuth ? <Home /> : <Redirect to="/" />}
+          </Route>
           <Route exact path="/create-playlist">
-            {isAuth ? <CreatePlaylist /> : <Redirect to="/" /> }
+            {isAuth ? <CreatePlaylist /> : <Redirect to="/" />}
           </Route>
           <Route exact path="/">
             <LandingPage />
           </Route>
         </Switch>
-      </main>
+      </Grid>
     </BrowserRouter>
   );
 }
