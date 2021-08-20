5388bb6cfc4694dbf7a307613dec8b2598f0e042
andwiput <andyginting94@gmail.com>
2021-08-20 09:37:21 +0700

    add test utils and sample data

---
+++ src/test/sampleData.ts
@@ -0,0 +1,32 @@
+import { Track } from '../types/spotify'
+
+export const singleTrack: Track = {
+  album: {
+    external_urls: {
+      spotify: 'https://open.spotify.com/album/1GbtB4zTqAsyfZEsm1RZfx',
+    },
+    id: '1GbtB4zTqAsyfZEsm1RZfx',
+    images: [
+      {
+        height: 300,
+        url: 'https://i.scdn.co/image/ab67616d00001e02e319baafd16e84f0408af2a0',
+        width: 300,
+      },
+    ],
+    name: 'A Night At The Opera (2011 Remaster)',
+    uri: 'spotify:album:1GbtB4zTqAsyfZEsm1RZfx',
+  },
+  artists: [
+    {
+      external_urls: {
+        spotify: 'https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d',
+      },
+      id: '1dfeR4HaWDbWqFHLkxsg1d',
+      name: 'Queen',
+      uri: 'spotify:artist:1dfeR4HaWDbWqFHLkxsg1d',
+    },
+  ],
+  id: '4u7EnebtmKWzUH433cf5Qv',
+  name: 'Bohemian Rhapsody - Remastered 2011',
+  uri: 'spotify:track:4u7EnebtmKWzUH433cf5Qv',
+}
