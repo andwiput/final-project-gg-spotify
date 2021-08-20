7ae32c5f824c1c0ed550d076be5235023d15e761
andwiput <andyginting94@gmail.com>
2021-08-21 01:55:58 +0700

    fix landing page

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -11,22 +11,22 @@ const LandingPage = () => {
     <Box maxW="100vw">
       <Grid as={'main'} templateColumns="1fr 1fr" gap="5vw" minH="90vh" px={"2rem"}>
         <Flex direction={"column"} justify={"center"}>
-          <Heading as={"h1"} fontSize={"6xl"}>
+          <Heading as={"h1"} fontSize={"6xl"} textAlign={'center'}>
             Welcome to GenG Music !
           </Heading>
-          <Text my={4} color={"smoke.500"}>
+          <Text my={4} color={"smoke.500"} textAlign={'center'}>
             Search your favorite tracks and create your own playlist
           </Text>
-          <Box>
+          <Box alignSelf={'center'}>
             <Button size={"md"} leftIcon={<FaSpotify/>} onClick={authorize}>Login with Spotify</Button>
           </Box>
         </Flex>
         <Center>
-          <Image src={"https://picsum.photos/500/350"} borderRadius="md" />
+          <Image src={"https://picsum.photos/id/452/500/350"} borderRadius="md" />
         </Center>
       </Grid>
       <Center as={'footer'} h={"3rem"} bg={colorMode === 'light' ? 'smoke.300' : 'smoke.800'}>
-        with 💚 by Andy Dwi for Generasi Gigih Program
+        with 💚 for Generasi Gigih Program
       </Center>
     </Box>
   );
