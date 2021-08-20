1fef94fd44e38818b6e6c9fe5d95566b02f67962
andwiput <andyginting94@gmail.com>
2021-08-20 09:30:01 +0700

    fixing spotify types

--- src/types/spotify.ts
+++ src/types/spotify.ts
@@ -8,9 +8,9 @@ export interface Track {
     external_ids?:      ExternalIDS;
     external_urls?:     ExternalUrls;
     href?:              string;
-    id?:                string;
+    id:                string;
     is_local?:          boolean;
-    name?:              string;
+    name:              string;
     popularity?:        number;
     preview_url?:       string | null;
     track_number?:      number;
@@ -18,28 +18,66 @@ export interface Track {
     uri:               string;
 }
 
-export interface Tracks {
-    href?:     string;
-    items?:    Track[];
-    limit?:    number;
-    next?:     string;
-    offset?:   number;
-    previous?: string;
-    total?:    number;
+export interface TracksData{
+    href:     string;
+    items:    Track[];
+    limit:    number;
+    next:     string;
+    offset:   number;
+    previous: string;
+    total:    number;
 }
 
-export interface SearchResponse {
-    tracks?: Tracks;
+export interface TracksResponse {
+    tracks: TracksData;
+}
+
+export interface Album {
+    album_type?:             string;
+    artists?:                Artist[];
+    available_markets?:      string[];
+    external_urls:          ExternalUrls;
+    href?:                   string;
+    id:                     string;
+    images:                 Image[];
+    name:                   string;
+    release_date?:           string | Date;
+    release_date_precision?: string;
+    type?:                   string;
+    uri:                    string;
+}
+
+export interface AlbumsData {
+    href:     string;
+    items:    Album[];
+    limit:    number;
+    next:     string;
+    offset:   number;
+    previous: null;
+    total:    number;
+}
+
+export interface AlbumsResponse {
+    albums: AlbumsData;
+}
+
+export interface Artist {
+    external_urls: ExternalUrls;
+    href?:          string;
+    id:            string;
+    name:          string;
+    type?:          string;
+    uri?:           string;
 }
 
 export interface UserProfile {
     country?:       string;
-    display_name?:  string;
+    display_name:  string;
     email?:         string;
     external_urls?: ExternalUrls;
     followers?:     Followers;
     href?:          string;
-    id?:            string;
+    id:            string;
     images?:        Image[];
     product?:       string;
     type?:          string;
@@ -52,13 +90,13 @@ export interface PlaylistResponse {
     external_urls?: ExternalUrls;
     followers?:     Followers;
     href?:          string;
-    id?:            string;
-    images?:        any[];
-    name?:          string;
+    id:            string;
+    images:        Image[];
+    name:          string;
     owner?:         Owner;
     public?:        boolean;
     snapshot_id?:   string;
-    tracks?:        Tracks;
+    tracks?:        TracksData;
     type?:          string;
     uri?:           string;
 }
@@ -69,33 +107,9 @@ export interface Followers {
 }
 
 export interface Image {
-    height: number | null;
+    height: number;
     url:    string;
-    width:  number | null;
-}
-
-export interface Album {
-    album_type?:             string;
-    artists?:                Artist[];
-    available_markets?:      string[];
-    external_urls?:          ExternalUrls;
-    href?:                   string;
-    id:                     string;
-    images:                 Image[];
-    name:                   string;
-    release_date?:           string | Date;
-    release_date_precision?: string;
-    type?:                   string;
-    uri:                    string;
-}
-
-export interface Artist {
-    external_urls?: ExternalUrls;
-    href?:          string;
-    id?:            string;
-    name?:          string;
-    type?:          string;
-    uri?:           string;
+    width:  number;
 }
 
 export interface Owner {
